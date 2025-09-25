7e355ca570428cb5d3f54ce55c22c2e3c5e20aeb
gauradevibulbule863 <gauradevi.b@indictranstech.com>
2025-09-25 10:58:39 +0530

    stock leadger entry, purchase receipt,custom stock leadger repot

--- ekata/ekata/custom_scripts/stock_ledger_entry/stock_ledger_entry_py.py
+++ ekata/ekata/custom_scripts/stock_ledger_entry/stock_ledger_entry_py.py
@@ -4,12 +4,12 @@ from frappe.model.mapper import get_mapped_doc
 
 def validate(doc, method=None):
     if doc.voucher_type == "Purchase Receipt":
-        print("\n\nvoucher type ------------ ", doc.voucher_type)
+        # print("\n\nvoucher type ------------ ", doc.voucher_type)
         PRDoc = frappe.get_doc("Purchase Receipt", doc.voucher_no)
         if PRDoc:
-            print("\n\n PRDoc -------------------------------", PRDoc)
+            # print("\n\n PRDoc -------------------------------", PRDoc)
             if PRDoc.lot_no:
-                print("\n\n PRDoc lot_no---------------------", PRDoc.lot_no)
+                # print("\n\n PRDoc lot_no---------------------", PRDoc.lot_no)
                 doc.receipt_no = PRDoc.lot_no
             if PRDoc.season:
                 doc.season = PRDoc.season
@@ -19,11 +19,11 @@ def validate(doc, method=None):
                 doc.receipt_no_data = PRDoc.receipt_no
 
             for i in PRDoc.items:
-                print(
-                    "\n\n item ---------------------------------------",
-                    i.item_code,
-                    i.outturn_no,
-                )
+                # print(
+                #     "\n\n item ---------------------------------------",
+                #     i.item_code,
+                #     i.outturn_no,
+                # )
 
                 if i.item_code == doc.item_code:
                     # doc.outturn_no = i.outturn_no
@@ -44,13 +44,13 @@ def validate(doc, method=None):
                     doc.note = i.note
 
     if doc.voucher_type == "Stock Entry":
-        print("\n\nvoucher type ------------ ", doc.voucher_type)
+        # print("\n\nvoucher type ------------ ", doc.voucher_type)
         SEDoc = frappe.get_doc("Stock Entry", doc.voucher_no)
         if SEDoc:
-            print("\n\n PRDoc -----------", SEDoc)
+            # print("\n\n PRDoc -----------", SEDoc)
 
             for i in SEDoc.items:
-                print("\n\n item -----------", i.item_code, i.outturn_no)
+                # print("\n\n item -----------", i.item_code, i.outturn_no)
 
                 if i.item_code == doc.item_code:
                     print(i.outturn_no, i.receipt_no)
@@ -58,3 +58,37 @@ def validate(doc, method=None):
                     doc.receipt_no = i.receipt_no
                     doc.coffee_processing = i.coffee_processing_details
                     doc.season = i.season
+
+def after_insert(doc, method=None):
+    try:
+        lot_value = None
+        outturn_no = None
+
+
+        # Case 1: Stock Entry
+        if doc.voucher_type == "Stock Entry":
+            lot_value, outturn_no = frappe.db.get_value(
+                "Stock Entry Detail",
+                {"parent": doc.voucher_no, "name": doc.voucher_detail_no},
+                ["receipt_no","outturn_no"]
+            )
+
+        # Case 2: Purchase Receipt
+        elif doc.voucher_type == "Purchase Receipt":
+            lot_value = frappe.db.get_value(
+                "Purchase Receipt Item",
+                {"parent": doc.voucher_no, "name": doc.voucher_detail_no},
+                "custom_lot_no"
+            )
+
+
+        # Update SLE if we found a value
+        if lot_value:
+            frappe.db.set_value("Stock Ledger Entry", doc.name, "custom_lot_no", lot_value)
+        
+        if outturn_no:
+            print("\n\n\n\n\nouter====", outturn_no)
+            frappe.db.set_value("Stock Ledger Entry", doc.name, "custom_outturn_no_data", outturn_no)
+
+    except Exception:
+        frappe.log_error(frappe.get_traceback(), "SLE after_insert custom_lot_no update")
